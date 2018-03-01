4279b4b8e49535cc5922ab6254c951198c0f19b2
esteban <efmino@uc.cl>
2018-03-01 01:19:29 -0800

    two tests to go

--- contracts/BadAuction.sol
+++ contracts/BadAuction.sol
@@ -6,42 +6,79 @@ import "./AuctionInterface.sol";
 contract BadAuction is AuctionInterface {
 
 
-	/* Bid function, vulnerable to reentrency attack.
-	 * Must return true on successful send and/or bid,
-	 * bidder reassignment
-	 * Must return false on failure and send people
-	 * their funds back
-	 */
-	function bid() payable external returns (bool) {
-		// YOUR CODE HERE
-	}
+/* Bid function, vulnerable to reentrency attack.
+    * Must return true on successful send and/or bid,
+    * bidder reassignment
+    * Must return false on failure and send people
+    * their funds back
+    */
 
 
-	/* 	Reduce bid function. Vulnerable to attack.
-		Allows current highest bidder to reduce 
-		their bid by 1. Do NOT make changes here.
-		Instead notice the vulnerabilities, and
-		implement the function properly in GoodAuction.sol  */
-	
-	function reduceBid() external {
-	    if (highestBid >= 0) {
-	        highestBid = highestBid - 1;
-	        require(highestBidder.send(1));
-	    } else {
-	    	revert();
-	    }
-	}
+    function bid() external payable returns (bool) {
+        // YOUR CODE HERE
+
+        // reentrency attack, this function can be called again before
+        // the first invocation of the function was finished
+        if (msg.value > highestBid) {
+
+            if (highestBidder != 0) {
+                refundHighestBidder(); // record the refund that this user can claim
+            }
+
+            highestBidder = msg.sender;
+            highestBid = msg.value;
+            return true;
+        } else {
+            msg.sender.transfer(msg.value);
+            return false;
+        }
+    }
+
+    
+    function refundHighestBidder() private returns (bool) {
+        if (!highestBidder.send(highestBid)) {
+            return false;
+        }
+        return true;
+    }
+    
+    /* 	Reduce bid function. Vulnerable to attack.
+        Allows current highest bidder to reduce 
+        their bid by 1. Do NOT make changes here.
+        Instead notice the vulnerabilities, and
+        implement the function properly in GoodAuction.sol  */
+
+    function reduceBid() external {
+        if (highestBid >= 0) {
+            highestBid = highestBid - 1;
+            require(highestBidder.send(1));
+        } 
+    }
+
+    // underflow and check only the highestbidder is reducing
+    modifier canReduce() {
+        require(highestBidder == msg.sender);
+        _;
+    }
+
+    /* in any situation a bidder with a lower or the same bid
+    than the current higgest bidder shiuld have not effect on the contract */
+    modifier bidIsAccepted() {
+        
+        _;
+    }
 
 
-	/* 	Remember this fallback function
-		gets invoked if somebody calls a
-		function that does not exist in this
-		contract. But we're good people so we don't
-		want to profit on people's mistakes.
-		How do we send people their money back?  */
+    /* 	Remember this fallback function
+        gets invoked if somebody calls a
+        function that does not exist in this
+        contract. But we're good people so we don't
+        want to profit on people's mistakes.
+        How do we send people their money back?  */
 
-	function () payable {
-		// YOUR CODE HERE
-	}
+    function () payable {
+        // YOUR CODE HERE
+        msg.sender.transfer(msg.value);
+    }
 
 }
--- contracts/GoodAuction.sol
+++ contracts/GoodAuction.sol
@@ -5,57 +5,124 @@ import "./AuctionInterface.sol";
 /** @title GoodAuction */
 contract GoodAuction is AuctionInterface {
 
-	/* New data structure, keeps track of refunds owed */
-	mapping(address => uint) refunds;
+    /* New data structure, keeps track of refunds owed */
+    struct Refund {
+        uint funds;
+        bool initialized;
+    }
+
+    mapping(address => Refund) refunds;
+
+    /* 	Bid function, now shifted to pull paradigm
+    Must return true on successful send and/or bid, bidder
+    reassignment. Must return false on failure and 
+    allow people to retrieve their funds  */
 
 
-	/* 	Bid function, now shifted to pull paradigm
-		Must return true on successful send and/or bid, bidder
-		reassignment. Must return false on failure and 
-		allow people to retrieve their funds  */
-	function bid() payable external returns(bool) {
-		// YOUR CODE HERE
-	}
+    function bid() payable external returns(bool) {
 
-	/*  Implement withdraw function to complete new 
-	    pull paradigm. Returns true on successful 
-	    return of owed funds and false on failure
-	    or no funds owed.  */
-	function withdrawRefund() external returns(bool) {
-		// YOUR CODE HERE
-	}
+        // higher bid should displace last highest bid 
+        // if previous higher bidder were not poisoned
 
-	/*  Allow users to check the amount they are owed
-		before calling withdrawRefund(). Function returns
-		amount owed.  */
-	function getMyBalance() constant external returns(uint) {
-		return refunds[msg.sender];
-	}
+        // Be able to displace the previous highest bidder 
+        // if bidding at a good auction
+
+        // Not be able to displace the previous highest bidder 
+        // if bidding on a bad auction and the previous highest bidder was poisoned
+        if (msg.value > highestBid) {
+            if (highestBidder != 0) {
+                refunds[highestBidder].funds = highestBid; 
+                refunds[highestBidder].initialized = true; 
+            }
+
+            highestBid = msg.value;
+            highestBidder = msg.sender; 
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // pull over push transactions
+    function returnFunds() private returns (bool) {
+        if (!msg.sender.send(msg.value)) {
+            return false;
+        }
+        return true;
+    }
+
+    /*  Implement withdraw function to complete new 
+    pull paradigm. Returns true on successful 
+    return of owed funds and false on failure
+    or no funds owed.  */
+    function withdrawRefund() external returns(bool) {
+        // YOUR CODE HERE
+        if (!(refunds[msg.sender].initialized && returnFund())) {
+            return false;
+        }
+        return true;
+    }
+
+    function returnFund() private returns (bool) {
+        if (!msg.sender.send(refunds[msg.sender].funds)) {
+            return false;
+        }
+        return true;
+    }
+
+    /*  Allow users to check the amount they are owed
+    before calling withdrawRefund(). Function returns
+    amount owed.  */
+    function getMyBalance() external constant  returns(uint) {
+        if (refunds[msg.sender].initialized == true) {
+            return refunds[msg.sender].funds;
+        } else {
+            return 0;
+        }
+    }
+
+    /* in any situation a bidder with a lower or the same bid
+    than the current higgest bidder shiuld have not effect on the contract */
+    modifier bidIsAccepted() {
+        require(msg.value > highestBid);
+        _;
+    }
+
+    /* 	Consider implementing this modifier
+    and applying it to the reduceBid function 
+    you fill in below. */
+
+    // underflow and check only the highestbidder is reducing
+    modifier canReduce() {
+        require(msg.sender == highestBidder);
+        _;
+    }
 
 
-	/* 	Consider implementing this modifier
-		and applying it to the reduceBid function 
-		you fill in below. */
-	modifier canReduce() {
-		_;
-	}
+    /*  Rewrite reduceBid from BadAuction to fix
+    the security vulnerabilities. Should allow the
+    current highest bidder only to reduce their bid amount */
+
+    // should be protected against underflow
+
+    function reduceBid() external {
+        // bad auction doesn't check for the send to decrease bid
+        if (highestBidder == msg.sender && highestBid > 0 && highestBid - 1 >= 0) {
+            highestBidder.transfer(1);
+            highestBid -= 1;
+        }
+    }
 
 
-	/*  Rewrite reduceBid from BadAuction to fix
-		the security vulnerabilities. Should allow the
-		current highest bidder only to reduce their bid amount */
-	function reduceBid() external {}
+    /* 	Remember this fallback function
+    gets invoked if somebody calls a
+    function that does not exist in this
+    contract. But we're good people so we don't
+    want to profit on people's mistakes.
+    How do we send people their money back?  */
 
-
-	/* 	Remember this fallback function
-		gets invoked if somebody calls a
-		function that does not exist in this
-		contract. But we're good people so we don't
-		want to profit on people's mistakes.
-		How do we send people their money back?  */
-
-	function () payable {
-		// YOUR CODE HERE
-	}
+    function () payable {
+        msg.sender.transfer(msg.value);
+    }
 
 }
